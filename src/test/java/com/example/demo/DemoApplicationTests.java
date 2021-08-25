1775c2926f504e7b8cd28b06729b3348e603d50b
lwy123-wq <l2158223895@qq.com>
2021-08-25 23:33:26 +0800

    demo

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.dao.PersonDao;
+import com.example.demo.service.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
     @Autowired
     PersonDao personDao=new PersonDao();
+    @Autowired
+    UserServiceImpl userService=new UserServiceImpl();
     @Test
     void test() {
         /*Person person=new Person("aaa");
         //personDao.createP("ccc");
         personDao.addPerson(person,"ccc");*/
+        userService.insertUser("zhaosan","98768");
     }
 
 }
